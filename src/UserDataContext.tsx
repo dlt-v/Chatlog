6ca6e5d08460a8e11e801202087ca074810cb45f
Delta.v <tmm.michalski@gmail.com>
2021-09-27 18:22:20 +0200

    Create Context and Provider

--- src/UserDataContext.tsx
+++ src/UserDataContext.tsx
@@ -1,14 +1,23 @@
 import React, { useState, useEffect, createContext } from 'react';
 
-export const UserDataContext = createContext();
+export const UserDataContext = createContext({});
 
-export const UserDataProvider = (props) => {
+export interface User {
+    //TODO: clear code duplication from Login with App.tsx
+    avatar: number;
+    name: string;
+}
 
-    
+export const UserDataProvider = () => {
+    const [user, setUser] = useState<User>({
+        //By default user is not logged in
+        avatar: -1,
+        name: '',
+    });
 
     return (
-        <UserDataContext.Provider value={[tasks, setTasks]}>
-            {props.children}
-        </UserDataContext.Provider>
+        <UserDataContext.Provider
+            value={[user, setUser]}
+        ></UserDataContext.Provider>
     );
 };
