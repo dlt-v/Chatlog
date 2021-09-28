cbfefaffde5d8278e1854fcbde7aa3e40037e633
Tomek Michalski <tmm.michalski@gmail.com>
2021-09-28 16:29:07 +0200

    Implement Dropdown conditional rendering

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -7,8 +7,10 @@ import React from 'react';
 export const Dropdown: React.FC = () => {
     return (
         <div className="dropdown">
-            <div className="dropdown__item">Edit Profile</div>
-            <div className="dropdown__item">Logout</div>
+            <div className="dropdown__item">
+                <span>Edit Profile</span>
+            </div>
+            <div className="dropdown__item dropdown__item--bold">Logout</div>
         </div>
     );
 };
--- src/components/UserHeader.tsx
+++ src/components/UserHeader.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Dropdown } from './Dropdown';
 
 import av0 from '../styles/img/av-0.svg';
 
 export const UserHeader: React.FC = () => {
+    const [close, setClose] = useState(false);
     return (
-        <div className="main__userHeader">
-            <img src={av0} alt="avatar" />
-            <span className="main__userHeader__userName">Ammulah</span>
-            <span className="icon-options"></span>
+        <div>
+            <div className="main__userHeader">
+                <img src={av0} alt="avatar" />
+                <span className="main__userHeader__userName">Ammulah</span>
+                <span
+                    onClick={() => setClose(!close)}
+                    className={close ? 'icon-back' : 'icon-options'}
+                ></span>
+            </div>
+            {close && <Dropdown />}
         </div>
     );
 };
