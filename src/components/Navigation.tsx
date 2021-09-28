13837ee3f3a49e69e5dc45cc88c7f5bfe3c7b413
Dominik Obloza <main@dominikobloza.pl>
2021-09-28 17:16:40 +0200

    Navbar set current window from props

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 
-export const Navigation = () => {
+interface Current {
+    friends?: boolean;
+    search?: boolean;
+}
+
+export const Navigation: React.FC<Current> = (props) => {
+    const friends = props.friends;
+    const search = props.search;
+
+    // todo: create routing to friends-page, send-page and search-page
+
     return (
         <div className="main__navigation">
-            <div className="main__navigation__friends">
+            <div
+                className={`${
+                    friends
+                        ? 'main__navigation__friends--active'
+                        : 'main__navigation__friends'
+                }`}
+            >
                 <span className="icon-contacts"></span>
             </div>
             <div className="main__navigation__send">
                 <span className="icon-send"></span>
             </div>
-            <div className="main__navigation__search">
+            <div
+                className={`${
+                    search
+                        ? 'main__navigation__search--active'
+                        : 'main__navigation__search'
+                }`}
+            >
                 <span className="icon-search"></span>
             </div>
         </div>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,6 +5,7 @@ import { Navigation } from '../components/Navigation';
 export const Main: React.FC = () => {
     return (
         <div className="main">
+            <h1>This is main page</h1>
             <Navigation />
         </div>
     );
