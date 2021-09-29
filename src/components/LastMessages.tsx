113f3cfe1aef8111cb9cc5ab53ca4d0c4b789f27
Dominik Obloza <main@dominikobloza.pl>
2021-09-29 20:37:44 +0200

    move to login page when not logged in

--- src/components/LastMessages.tsx
+++ src/components/LastMessages.tsx
@@ -29,11 +29,13 @@ export const LastMessages: React.FC = () => {
     // Get user conversations
     const findUserConversations = (chatlogs: MessageState[]) => {
         let userConversations: MessageState[] = [];
+
         chatlogs.filter((chat: any) => {
             if (chat.participants.includes(user.id)) {
                 userConversations = [chat, ...userConversations];
             }
         });
+
         return userConversations;
     };
 
@@ -77,80 +79,80 @@ export const LastMessages: React.FC = () => {
         fetchConversations();
     }, []);
 
-    const last = [
-        {
-            id: 1,
-            avatar: 1,
-            name: 'Kalafior',
-            time: '12:50',
-            message: 'siema co tam u ciebie',
-            unread: 3,
-        },
-        {
-            id: 2,
-            avatar: 2,
-            name: 'Baran',
-            time: '11:50',
-            message: 'co tam',
-            unread: 1,
-        },
-        {
-            id: 3,
-            avatar: 1,
-            name: 'Koniczyna',
-            time: '18:20',
-            message: 'witam serdecznie, skad sie biora takie zwierzeta jak ty?',
-            unread: 0,
-        },
-        {
-            id: 4,
-            avatar: 3,
-            name: 'Kalafior',
-            time: '12:50',
-            message: 'siema co tam u ciebie',
-            unread: 3,
-        },
-        {
-            id: 5,
-            avatar: 2,
-            name: 'Baran',
-            time: '11:50',
-            message: 'co tam',
-            unread: 1,
-        },
-        {
-            id: 6,
-            avatar: 3,
-            name: 'Koniczyna',
-            time: '18:20',
-            message: 'witam serdecznie, skad sie biora takie zwierzeta jak ty?',
-            unread: 0,
-        },
-        {
-            id: 7,
-            avatar: 1,
-            name: 'Kalafior',
-            time: '12:50',
-            message: 'siema co tam u ciebie',
-            unread: 3,
-        },
-        {
-            id: 8,
-            avatar: 2,
-            name: 'Baran',
-            time: '11:50',
-            message: 'co tam',
-            unread: 1,
-        },
-        {
-            id: 9,
-            avatar: 0,
-            name: 'Koniczyna',
-            time: '18:20',
-            message: 'witam serdecznie, skad sie biora takie zwierzeta jak ty?',
-            unread: 0,
-        },
-    ];
+    // const last = [
+    //     {
+    //         id: 1,
+    //         avatar: 1,
+    //         name: 'Kalafior',
+    //         time: '12:50',
+    //         message: 'siema co tam u ciebie',
+    //         unread: 3,
+    //     },
+    //     {
+    //         id: 2,
+    //         avatar: 2,
+    //         name: 'Baran',
+    //         time: '11:50',
+    //         message: 'co tam',
+    //         unread: 1,
+    //     },
+    //     {
+    //         id: 3,
+    //         avatar: 1,
+    //         name: 'Koniczyna',
+    //         time: '18:20',
+    //         message: 'witam serdecznie, skad sie biora takie zwierzeta jak ty?',
+    //         unread: 0,
+    //     },
+    //     {
+    //         id: 4,
+    //         avatar: 3,
+    //         name: 'Kalafior',
+    //         time: '12:50',
+    //         message: 'siema co tam u ciebie',
+    //         unread: 3,
+    //     },
+    //     {
+    //         id: 5,
+    //         avatar: 2,
+    //         name: 'Baran',
+    //         time: '11:50',
+    //         message: 'co tam',
+    //         unread: 1,
+    //     },
+    //     {
+    //         id: 6,
+    //         avatar: 3,
+    //         name: 'Koniczyna',
+    //         time: '18:20',
+    //         message: 'witam serdecznie, skad sie biora takie zwierzeta jak ty?',
+    //         unread: 0,
+    //     },
+    //     {
+    //         id: 7,
+    //         avatar: 1,
+    //         name: 'Kalafior',
+    //         time: '12:50',
+    //         message: 'siema co tam u ciebie',
+    //         unread: 3,
+    //     },
+    //     {
+    //         id: 8,
+    //         avatar: 2,
+    //         name: 'Baran',
+    //         time: '11:50',
+    //         message: 'co tam',
+    //         unread: 1,
+    //     },
+    //     {
+    //         id: 9,
+    //         avatar: 0,
+    //         name: 'Koniczyna',
+    //         time: '18:20',
+    //         message: 'witam serdecznie, skad sie biora takie zwierzeta jak ty?',
+    //         unread: 0,
+    //     },
+    // ];
 
     return (
         <div className="lastMessages">
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router';
 
 import { UserHeader } from '../components/UserHeader';
 import { Navigation } from '../components/Navigation';
 import { LastMessages } from '../components/LastMessages';
+import { UserDataContext } from '../UserDataContext';
 
 export const Main: React.FC = () => {
+    const { user } = useContext(UserDataContext);
+    let history = useHistory();
+
+    useEffect(() => {
+        if (user.id === -1) history.push('/login');
+    }, []);
+
     return (
         <div className="main">
             <UserHeader />
