49212fdd8eaa84bd3b094b3a4157af179aac24e1
Dominik Obloza <main@dominikobloza.pl>
2021-10-03 17:41:37 +0200

    implement input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Input: React.FC = () => {
+    const [text, setText] = useState<string>('');
+    const [rows, setrows] = useState<number>(1);
+
+    const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setText(e.target.value);
+        let textWidth = e.target.value.length;
+        if (textWidth % 33 === 0) setrows(rows + 1);
+    };
+
     return (
         <div className="input">
-            <input
-                type="text"
+            <span
+                role="textbox"
+                contentEditable="true"
+                onChange={handleInput}
                 className="input__field"
-                placeholder="say hello"
-            />
+                aria-label="say hello"
+                title="say hello"
+                aria-placeholder="siema"
+            ></span>
             <span className="icon-send"></span>
         </div>
     );
