7df07e7fd2c4172ae467e43c3de290fa946dcfea
Tomek Michalski <tmm.michalski@gmail.com>
2021-10-04 10:27:08 +0200

    Add input styling

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import { Navigation } from '../components/Navigation';
+import { UserHeader } from '../components/UserHeader';
+import { UserCard } from '../components/UserCard';
 
 interface Props {
     friends: boolean;
@@ -8,7 +10,18 @@ interface Props {
 export const Search: React.FC<Props> = (props) => {
     return (
         <div className="search">
-            <h2>This is a {props.friends ? 'friends' : 'user search'} page</h2>
+            <UserHeader />
+            {props.friends && <h2 className="search__header">Friends list</h2>}
+            <input
+                className="search__input"
+                type="text"
+                placeholder={props.friends ? 'Search friends' : 'Search users'}
+            />
+            <div className="search__container">
+                <UserCard />
+                <UserCard />
+                <UserCard />
+            </div>
             <Navigation search={!props.friends} friends={props.friends} />
         </div>
     );
