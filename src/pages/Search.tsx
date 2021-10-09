e841d421ca4cd6395258ee59311b8a723c412eff
Tomek Michalski <tmm.michalski@gmail.com>
2021-10-09 15:18:08 +0200

    Create UserCard Component

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
+import { UserDataContext } from '../UserDataContext';
 
 import { Navigation } from '../components/Navigation';
 import { UserHeader } from '../components/UserHeader';
 import { UserCard } from '../components/UserCard';
 
+// waiting mechanism for user input
+// only search for non-friend users if the input is not empty
+// show up friend users immidately but also have an option to filter the users by name
+
 interface Props {
     friends: boolean;
 }
 export const Search: React.FC<Props> = (props) => {
+    const { user } = useContext(UserDataContext);
+    const [friendList, setFriendList] = useState<number[]>([]);
+    const [userList, setUserList] = useState<any[]>([]);
+    const [input, setInput] = useState<string>('');
+
+    const fetchFriends = async () => {
+        if (props.friends) {
+            const result = await fetch(
+                `http://localhost:3001/users/${user.id}`
+            ).then((response) => response.json());
+            setFriendList(result.friends);
+        }
+    };
+
+    const fetchUsers = async () => {};
+
+    useEffect(() => {
+        if (props.friends) fetchFriends();
+    }, []);
+
     return (
         <div className="search">
             <UserHeader />
             {props.friends && <h2 className="search__header">Friends list</h2>}
             <input
                 className="search__input"
-                type="text"
+                type="search"
                 placeholder={props.friends ? 'Search friends' : 'Search users'}
             />
             <div
@@ -24,9 +49,9 @@ export const Search: React.FC<Props> = (props) => {
                         : 'search__container'
                 }
             >
-                <UserCard id={1} name="Pepe" avatar={1} />
-                <UserCard id={2} name="Fineasz" avatar={1} />
-                <UserCard id={3} name="Ferb" avatar={1} />
+                {friendList.map((friend) => (
+                    <UserCard id={friend} key={friend} />
+                ))}
             </div>
             <Navigation search={!props.friends} friends={props.friends} />
         </div>
